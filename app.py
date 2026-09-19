2ba20398b6ae886d615aa7f97615240acd125483
paoesma95 <paola.espanam@upb.edu.co>
2026-09-19 12:03:48 -0500

    Fix filename path for model loading

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import matplotlib.pyplot as plt
 
 #Cargamos el modelo
 import pickle
-filename = '/modelo-reg.pkl'
+filename = 'modelo-reg.pkl'
 modelo, min_max_scaler, variables = pickle.load(open(filename, 'rb'))
 modelo
 
